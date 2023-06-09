f9a7f56a4d2ba5d4a39ab784dbbf3f7f95a99812
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-09 12:56:46 +0100

    feat(pdf-generator): add total

--- pdf-generator/main.js
+++ pdf-generator/main.js
@@ -8,19 +8,25 @@ module.exports = async ({ res }) => {
 };
 
 function generateFakeOrder() {
+  const items = Array.from(
+    { length: faker.number.int({ min: 1, max: 5 }) },
+    () => ({
+      description: faker.commerce.productName(),
+      quantity: faker.number.int({ min: 1, max: 10 }),
+      cost: faker.commerce.price(),
+    })
+  );
+
   return {
     id: faker.string.uuid(),
     date: faker.date.past(),
     name: faker.person.fullName(),
-    items: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => ({
-      description: faker.commerce.productName(),
-      quantity: faker.number.int({ min: 1, max: 10 }),
-      cost: faker.commerce.price(),
-    })),
+    items,
+    total: items.reduce((acc, { cost }) => acc + cost, 0),
   };
 }
 
-async function createPdf({ id, date, name, items }) {
+async function createPdf({ id, date, name, items, total }) {
   const document = await PDFDocument.create();
   const page = document.addPage(PageSizes.A4); //[595.28, 841.89]
 
@@ -51,6 +57,7 @@ async function createPdf({ id, date, name, items }) {
     .join("\n");
 
   page.drawText(orderList, { x: 50, y: 450, size: 25 });
+  page.drawText(`Total: $${total}`, { x: 50, y: 400, size: 25 });
 
   const pdfBytes = await document.save();
 
