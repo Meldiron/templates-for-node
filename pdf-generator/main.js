44b369899e35848cddd713c1cac16bd6ff53bdae
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-09 12:25:58 +0100

    feat(pdf-generator): use fake data

--- pdf-generator/main.js
+++ pdf-generator/main.js
@@ -1,17 +1,52 @@
 const { PDFDocument, PageSizes } = require("pdf-lib");
+import { faker } from "@faker-js/faker";
 
-async function createPdf({ name, items }) {
+module.exports = async ({ res }) => {
+  const fakeOrder = generateFakeOrder();
+  const pdfBuffer = await createPdf(fakeOrder);
+  return res.send(pdfBuffer, 200, { "Content-Type": "application/pdf" });
+};
+
+function generateFakeOrder() {
+  return {
+    id: faker.string.uuid(),
+    date: faker.date.past(),
+    name: faker.person.fullName(),
+    items: Array.from({ length: faker.number.int({ min: 1, max: 5 }) }, () => ({
+      description: faker.commerce.productName(),
+      quantity: faker.number.int({ min: 1, max: 10 }),
+      cost: faker.commerce.price(),
+    })),
+  };
+}
+
+async function createPdf({ id, date, name, items }) {
   const document = await PDFDocument.create();
   const page = document.addPage(PageSizes.A4); //[595.28, 841.89]
 
-  page.drawText(new Date().toLocaleDateString(), { x: 50, y: 800, size: 25 });
+  page.drawText("Sample Invoice", { x: 50, y: 800, size: 50 });
+  page.drawText(new Date(date).toLocaleDateString(), {
+    x: 500,
+    y: 800,
+    size: 25,
+  });
 
-  page.drawText(`Hello ${name}!`, { x: 50, y: 700, size: 50 });
+  page.drawText(`Hello ${name}!`, {
+    x: 50,
+    y: 700,
+    size: 50,
+  });
+
+  page.drawText(`Order ID: ${id}`, {
+    x: 50,
+    y: 650,
+    size: 25,
+  });
 
   const orderList = items
     .map(
       ({ description, quantity, cost }) =>
-        `${description} x ${quantity} = ${cost}`
+        `${description} x ${quantity} = $${cost}`
     )
     .join("\n");
 
@@ -21,14 +56,3 @@ async function createPdf({ name, items }) {
 
   return Buffer.from(pdfBytes.buffer);
 }
-
-module.exports = async ({ req, res, log, error }) => {
-  if (!req.body) {
-    error("Invalid request body");
-    return res.json({ ok: false, msg: "Invalid request body" }, 400);
-  }
-
-  const pdfBuffer = await createPdf(req.body);
-
-  return res.send(pdfBuffer, 200, { "Content-Type": "application/pdf" });
-};
