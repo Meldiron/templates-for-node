a9ea4c04a2bd27131a7a53d2ed7fe96436b6aa6c
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-12 13:57:39 +0100

    feat(html-form-email): send email

--- html-form-email/main.js
+++ html-form-email/main.js
@@ -1,6 +1,6 @@
 const querystring = require("querystring");
 
-module.exports = async ({ req, res }) => {
+module.exports = async ({ req, res, error }) => {
   console.log("Hello, World! 👋");
 
   if (req.method !== "post") {
@@ -23,9 +23,71 @@ module.exports = async ({ req, res }) => {
     );
   }
 
+  const SMTP_URL = process.env.SMTP_URL;
+  const SMTP_PORT = process.env.SMTP_PORT;
+  const SMTP_USERNAME = process.env.SMTP_USERNAME;
+  const SMTP_PASSWORD = process.env.SMTP_PASSWORD;
+  if (!SMTP_URL || !SMTP_PORT || !SMTP_USERNAME || !SMTP_PASSWORD) {
+    error("Missing SMTP credentials.");
+    return res.json(
+      {
+        ok: false,
+        msg: "Internal server error",
+      },
+      500
+    );
+  }
+
+  const INBOUND_EMAIL = process.env.INBOUND_EMAIL;
+  if (!INBOUND_EMAIL) {
+    error("Missing INBOUND_EMAIL.");
+    return res.json(
+      {
+        ok: false,
+        msg: "Internal server error",
+      },
+      500
+    );
+  }
+
+  const { name, email, message } = querystring.parse(req.body);
+  if (!name || !email || !message) {
+    return res.json(
+      {
+        ok: false,
+        msg: "Missing required fields",
+      },
+      400
+    );
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: SMTP_URL,
+    port: SMTP_PORT,
+    auth: {
+      user: SMTP_USERNAME,
+      pass: SMTP_PASSWORD,
+    },
+  });
+
+  const body = `Name: ${name}
+Email: ${email}
+Message: ${message}
+    `;
+
+  await transporter.sendMail({
+    from: `"${name}" <${email}>`,
+    to: INBOUND_EMAIL,
+    subject: "Form submission",
+    body,
+  });
+
   return res.json({
-    query: req.query,
-    body: querystring.parse(req.body),
-    headers: req.headers,
+    ok: true,
+    msg: `Success! Your message has been delivered.`,
   });
 };
+
+function emailTemplate({ name, email, message }) {
+  return;
+}
