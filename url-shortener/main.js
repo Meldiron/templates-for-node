5d9eca68f8ae581083d4f8ad667d1842ba84e331
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-13 13:59:13 +0100

    feat(url-shortener): setup function

--- url-shortener/main.js
+++ url-shortener/main.js
@@ -4,9 +4,12 @@ const REQUIRED_VARIABLES = [
   "APPWRITE_API_KEY",
   "APPWRITE_ENDPOINT",
   "APPWRITE_PROJECT_ID",
-  "APPWRITE_DATABASE_ID",
-  "APPWRITE_URL_COLLECTION_ID",
 ];
+
+const DATABASE_ID = process.env.DATABASE_ID ?? "url-shortener";
+const DATABASE_NAME = "URL Shortener";
+const COLLECTION_ID = process.env.COLLECTION_ID ?? "urls";
+const COLLECTION_NAME = "URLs";
 
 module.exports = async ({ res, req, log, error }) => {
   const variables = validateEnvironment();
@@ -18,12 +21,12 @@ module.exports = async ({ res, req, log, error }) => {
   }
 
   const client = new sdk.Client();
-  const databases = new sdk.Databases(client);
-
   client
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
     .setProject(process.env.APPWRITE_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
+
+  await setupDatabase(databases);
 
   return res.json({
     path: req.path,
@@ -40,4 +43,28 @@ function validateEnvironment() {
   return {
     missing,
   };
+}
+
+async function setupDatabase(databases) {
+  try {
+    await databases.create(DATABASE_ID, DATABASE_NAME);
+    await databases.createCollection(
+      DATABASE_ID,
+      COLLECTION_ID,
+      COLLECTION_NAME
+    );
+    await Promise.all([
+      databases.createUrlAttribute(
+        DATABASE_ID,
+        COLLECTION_ID,
+        "original",
+        true
+      ),
+      databases.createUrlAttribute(DATABASE_ID, COLLECTION_ID, "short", true),
+    ]);
+  } catch (error) {
+    if (error.code !== 409) {
+      throw error;
+    }
+  }
 }
