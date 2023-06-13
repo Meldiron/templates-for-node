ffbe1449e829a4c45fa2ebd12274d74f7374f522
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-13 14:45:54 +0100

    feat(url-shortener): redirect functionality

--- url-shortener/env.d.ts
+++ url-shortener/env.d.ts
@@ -4,6 +4,7 @@ declare global {
       APPWRITE_ENDPOINT?: string;
       APPWRITE_PROJECT_ID?: string;
       APPWRITE_API_KEY?: string;
+      SHORT_DOMAIN?: string;
     }
   }
 }
--- url-shortener/main.js
+++ url-shortener/main.js
@@ -1,9 +1,12 @@
-const sdk = require("node-appwrite");
+const { nanoid } = require("nanoid");
+const { default: sdk, Unique } = require("node-appwrite");
+const querystring = require("node:querystring");
 
 const REQUIRED_VARIABLES = [
   "APPWRITE_API_KEY",
   "APPWRITE_ENDPOINT",
   "APPWRITE_PROJECT_ID",
+  "SHORT_DOMAIN",
 ];
 
 const DATABASE_ID = process.env.DATABASE_ID ?? "url-shortener";
@@ -31,12 +34,46 @@ module.exports = async ({ res, req, log, error }) => {
   const created = await setupDatabase(databases);
   log(`Database ${created ? "created" : "already exists"}.`);
 
-  return res.json({
-    path: req.path,
-    url: req.url,
-    query: req.query,
-    body: req.body,
-  });
+  if (isFormRequest(req)) {
+    const { url } = querystring.parse(req.body);
+    if (!url) {
+      error("Missing required parameter: url");
+      throw new Error("Missing required parameter: url");
+    }
+
+    log(`Creating document in collection ${COLLECTION_ID}...`);
+    const document = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID,
+      nanoid(6),
+      {
+        original: url,
+      }
+    );
+
+    return res.send(
+      `Link created: ${new URL(
+        document.$id,
+        process.env.SHORT_DOMAIN
+      ).toString()}`
+    );
+  }
+
+  // Remove leading and trailing slashes
+  const shortId = req.path.replace(/^\/|\/$/g, "");
+
+  try {
+    const document = await databases.getDocument(
+      DATABASE_ID,
+      COLLECTION_ID,
+      shortId
+    );
+
+    return res.redirect(302, document.original);
+  } catch (error) {
+    if (error.code !== 404) throw error;
+    return res.send(`404: Not found.`);
+  }
 };
 
 function validateEnvironment() {
@@ -48,6 +85,13 @@ function validateEnvironment() {
   };
 }
 
+function isFormRequest(req) {
+  return (
+    req.headers["content-type"] === "application/x-www-form-urlencoded" &&
+    req.method === "post"
+  );
+}
+
 async function setupDatabase(databases) {
   try {
     await databases.create(DATABASE_ID, DATABASE_NAME);
@@ -56,15 +100,12 @@ async function setupDatabase(databases) {
       COLLECTION_ID,
       COLLECTION_NAME
     );
-    await Promise.all([
-      databases.createUrlAttribute(
-        DATABASE_ID,
-        COLLECTION_ID,
-        "original",
-        true
-      ),
-      databases.createUrlAttribute(DATABASE_ID, COLLECTION_ID, "short", true),
-    ]);
+    await databases.createUrlAttribute(
+      DATABASE_ID,
+      COLLECTION_ID,
+      "original",
+      true
+    );
   } catch (error) {
     // If database already exists, we can ignore the error
     if (error.code !== 409) throw error;
