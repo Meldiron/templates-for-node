779d3449fde674dafc8d722d9aea081fd67049a4
loks0n <22452787+loks0n@users.noreply.github.com>
2023-06-30 23:17:04 -0400

    chore(stripe-subscriptions): use service pattern

--- stripe-subscriptions/src/main.js
+++ stripe-subscriptions/src/main.js
@@ -1,27 +1,15 @@
 /// <reference types="stripe-event-types" />
 
 const stripe = require("stripe");
-const { Client, Databases } = require("node-appwrite");
+const AppwriteService = require("./appwrite");
 const getEnvironment = require("./environment");
 
 module.exports = async ({ req, res, log, error }) => {
-  const {
-    STRIPE_SECRET_KEY,
-    APPWRITE_ENDPOINT,
-    APPWRITE_PROJECT_ID,
-    APPWRITE_API_KEY,
-    CANCEL_URL,
-  } = getEnvironment();
+  const { STRIPE_SECRET_KEY, CANCEL_URL } = getEnvironment();
 
-  const client = new Client();
-  client
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID)
-    .setKey(APPWRITE_API_KEY);
+  const appwrite = AppwriteService();
 
-  const databases = new Databases(client);
-
-  // Note: stripe API types are faulty
+  // Note: stripe cjs API types are faulty
   /** @type {import('stripe').Stripe} */
   // @ts-ignore
   const stripeClient = stripe(STRIPE_SECRET_KEY);
@@ -29,12 +17,6 @@ module.exports = async ({ req, res, log, error }) => {
   switch (req.path) {
     case "/checkout":
       const userId = req.headers["x-appwrite-user-id"];
-
-      log("Headers:");
-      req.headers.forEach((value, key) => {
-        log(`${key} = ${value}`);
-      });
-
       if (!userId) {
         error("User ID not found in request.");
         return res.redirect(CANCEL_URL, 303);
@@ -50,24 +32,33 @@ module.exports = async ({ req, res, log, error }) => {
 
     case "/webhook":
       const event = validateWebhookRequest(stripeClient, req);
-      if (!event) return res.empty();
+      if (!event) return res.json({ success: false }, 401);
 
       if (event.type === "customer.subscription.created") {
         const session = event.data.object;
         const userId = session.metadata.userId;
 
-        await upgradeUser(databases, userId);
-        log(`Subscription created for user ${userId}.`);
+        if (await appwrite.hasSubscription(userId)) {
+          error(`Subscription already exists - skipping`);
+          return res.json({ success: true });
+        }
+
+        await appwrite.createSubscription(userId);
       }
+
       if (event.type === "customer.subscription.deleted") {
         const session = event.data.object;
         const userId = session.metadata.userId;
 
-        await downgradeUser(databases, userId);
-        log(`Subscription deleted for user ${userId}.`);
+        if (!(await appwrite.hasSubscription(userId))) {
+          error(`Subscription does not exist - skipping`);
+          return res.json({ success: true });
+        }
+
+        await appwrite.deleteSubscription(userId);
       }
 
-      return res.empty();
+      return res.json({ success: true });
 
     default:
       return res.send("Not Found", 404);
@@ -125,47 +116,5 @@ function validateWebhookRequest(stripeClient, req) {
     return /** @type {import("stripe").Stripe.DiscriminatedEvent} */ (event);
   } catch (err) {
     return null;
-  }
-}
-
-/**
- * @param {Databases} databases
- * @param {string} userId
- */
-async function upgradeUser(databases, userId) {
-  const { DATABASE_ID, COLLECTION_ID } = getEnvironment();
-
-  if (!(await hasExistingSubscription(databases, userId))) {
-    await databases.createDocument(DATABASE_ID, COLLECTION_ID, userId, {
-      subscriptionType: "premium",
-    });
-  }
-}
-
-/**
- * @param {Databases} databases
- * @param {string} userId
- */
-async function downgradeUser(databases, userId) {
-  const { DATABASE_ID, COLLECTION_ID } = getEnvironment();
-
-  if (await hasExistingSubscription(databases, userId)) {
-    await databases.deleteDocument(DATABASE_ID, COLLECTION_ID, userId);
-  }
-}
-
-/**
- * @param {Databases} databases
- * @param {string} userId
- */
-async function hasExistingSubscription(databases, userId) {
-  const { DATABASE_ID, COLLECTION_ID } = getEnvironment();
-
-  try {
-    await databases.getDocument(DATABASE_ID, COLLECTION_ID, userId);
-    return true;
-  } catch (err) {
-    if (err.code !== 404) throw err;
-    return false;
   }
 }
--- stripe-subscriptions/src/setup.js
+++ stripe-subscriptions/src/setup.js
@@ -1,66 +1,17 @@
-const { Client, Databases } = require("node-appwrite");
-const getEnvironment = require("./environment");
+const AppwriteService = require("./appwrite");
 
 async function setup() {
   console.log("Executing setup script...");
 
-  const {
-    APPWRITE_ENDPOINT,
-    APPWRITE_PROJECT_ID,
-    APPWRITE_API_KEY,
-    DATABASE_ID,
-  } = getEnvironment();
+  const appwrite = AppwriteService();
 
-  const client = new Client();
-  client
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID)
-    .setKey(APPWRITE_API_KEY);
-
-  const databases = new Databases(client);
-
-  try {
-    await databases.get(DATABASE_ID);
+  if (await appwrite.doesSubscribersDatabaseExist()) {
     console.log(`Database exists.`);
-  } catch (err) {
-    // If the database does not exist, we can create it
-    if (err.code !== 404) throw err;
-    await setupDatabase(databases);
-    console.log(`Database created.`);
+    return;
   }
-}
 
-/**
- * @param {Databases} databases
- */
-async function setupDatabase(databases) {
-  const { DATABASE_ID, DATABASE_NAME, COLLECTION_ID, COLLECTION_NAME } =
-    getEnvironment();
-  try {
-    await databases.create(DATABASE_ID, DATABASE_NAME);
-    await databases.createCollection(
-      DATABASE_ID,
-      COLLECTION_ID,
-      COLLECTION_NAME
-    );
-    await databases.createStringAttribute(
-      DATABASE_ID,
-      COLLECTION_ID,
-      "userId",
-      255,
-      true
-    );
-    await databases.createStringAttribute(
-      DATABASE_ID,
-      COLLECTION_ID,
-      "subscriptionType",
-      255,
-      true
-    );
-  } catch (err) {
-    // If resource already exists, we can ignore the error
-    if (err.code !== 409) throw err;
-  }
+  await appwrite.setupSubscribersDatabase();
+  console.log(`Database created.`);
 }
 
 setup();
