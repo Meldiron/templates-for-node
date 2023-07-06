d7e2c679e44508493c1d87622812fc1befd074a9
Matej Bačo <matejbaco2000@gmail.com>
2023-07-06 11:58:45 +0200

    Comment verification

--- whatsapp-with-vonage/src/main.js
+++ whatsapp-with-vonage/src/main.js
@@ -20,14 +20,14 @@ module.exports = async ({ req, res, log }) => {
     return res.send(html, 200, { "Content-Type": "text/html; charset=utf-8" });
   }
 
-  const token = (req.headers.authorization ?? "").split(" ")[1];
-  var decoded = jwt.verify(token, VONAGE_API_SIGNATURE_SECRET, {
-    algorithms: ["HS256"],
-  });
+  // const token = (req.headers.authorization ?? "").split(" ")[1];
+  // var decoded = jwt.verify(token, VONAGE_API_SIGNATURE_SECRET, {
+  //   algorithms: ["HS256"],
+  // });
 
-  if (sha256(req.bodyString) != decoded["payload_hash"]) {
-    throw new Error("Invalid signature.");
-  }
+  // if (sha256(req.bodyString) != decoded["payload_hash"]) {
+  //   throw new Error("Invalid signature.");
+  // }
 
   const from = req.body.from;
 
