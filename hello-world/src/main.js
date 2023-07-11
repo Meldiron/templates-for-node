341d75d66d94de4eb02afe47e5614956e811febc
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-11 17:04:16 +0100

    chore add comment

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,4 +1,22 @@
-module.exports = async ({ res, log }) => {
+const fs = require("fs");
+const path = require("path");
+
+const staticFolder = path.join(__dirname, "../static");
+
+// comment
+
+module.exports = async ({ req, res, log }) => {
   log("Hello, World! 👋");
-  return res.send("Hello, World! 👋");
+
+  if (req.method === "GET") {
+    let html = fs
+      .readFileSync(path.join(staticFolder, "index.html"))
+      .toString();
+
+    return res.send(html, 200, { "Content-Type": "text/html; charset=utf-8" });
+  }
+
+  return res.json({
+    message: "Hello, World! 👋",
+  });
 };
